907769ab40c37d19d52b58531fdcfefe742495d4
anguzudouglas <douuglas.anguzu@gmail.com>
2026-05-19 15:10:00 +0300

    Add configuration constants for execution and imports

---
+++ app/config.py
@@ -0,0 +1,15 @@
+EXECUTION_TIMEOUT = 15
+
+BLOCKED_IMPORTS = [
+    "os",
+    "sys",
+    "subprocess",
+    "socket",
+    "shutil",
+    "requests",
+    "multiprocessing",
+    "ctypes",
+    "pathlib"
+]
+
+MAX_ARTIFACT_SIZE_MB = 5
