5a7e15fa2a526ca444580f8c10326ca588c77c17
anguzudouglas <douuglas.anguzu@gmail.com>
2026-05-19 19:29:03 +0300

    Refactor FastAPI app to use Jinja2 templates

--- app/main.py
+++ app/main.py
@@ -1,62 +1,14 @@
-import os
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
-from fastapi.responses import HTMLResponse
+templates = Jinja2Templates(directory="templates")
 
-from app.models import ExecuteRequest
-from app.executor import execute_code
-from app.cleanup import cleanup_artifacts
-from app.docs_page import get_docs_html
+@app.get("/")
+async def docs_home(request: Request):
 
-
-app = FastAPI(
-    title="Python Execution API"
-)
-
-
-@app.get("/", response_class=HTMLResponse)
-async def root():
-
-    return get_docs_html()
-
-
-@app.post("/execute")
-async def execute(request: ExecuteRequest):
-
-    # cleanup expired artifacts
-    cleanup_artifacts()
-
-    result = execute_code(
-        code=request.code,
-        timeout=request.timeout,
-        files=request.files
-    )
-
-    return result
-
-
-@app.get("/artifact/{artifact_id}/{filename}")
-async def get_artifact(
-    artifact_id: str,
-    filename: str
-):
-
-    path = os.path.join(
-        "storage",
-        "artifacts",
-        artifact_id,
-        filename
-    )
-
-    if not os.path.exists(path):
-
-        return {
-            "success": False,
-            "status_code": 404,
-            "error": {
-                "message": "Artifact not found"
-            }
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request
         }
-
-    return FileResponse(path)
+    )
