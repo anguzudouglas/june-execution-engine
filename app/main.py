d693c09dabe7220efee2b6ea4c244f6fb9ec5b80
anguzudouglas <douuglas.anguzu@gmail.com>
2026-05-19 19:34:28 +0300

    Set up FastAPI with static files and routers
    Refactor FastAPI application structure by adding static files and routers.

--- app/main.py
+++ app/main.py
@@ -1,14 +1,31 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi import Request
 
-templates = Jinja2Templates(directory="templates")
+from app.routes.docs import router as docs_router
+from app.routes.execute import router as execute_router
+from app.routes.uploads import router as uploads_router
+from app.routes.artifacts import router as artifacts_router
 
-@app.get("/")
-async def docs_home(request: Request):
 
-    return templates.TemplateResponse(
-        "index.html",
-        {
-            "request": request
-        }
-    )
+app = FastAPI(
+    title="June Python Sandbox"
+)
+
+# static assets
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
+)
+
+# templates
+templates = Jinja2Templates(
+    directory="app/templates"
+)
+
+# routers
+app.include_router(docs_router)
+app.include_router(execute_router)
+app.include_router(uploads_router)
+app.include_router(artifacts_router)
