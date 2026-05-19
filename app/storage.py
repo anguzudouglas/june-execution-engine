28c0f9c1615a1423d79a4759ee4a2c6ffa7c3727
anguzudouglas <douuglas.anguzu@gmail.com>
2026-05-19 15:42:13 +0300

    Implement artifact storage functionality

---
+++ app/storage.py
@@ -0,0 +1,25 @@
+import os
+import uuid
+import shutil
+from datetime import datetime, timedelta
+
+BASE_DIR = "storage/artifacts"
+
+os.makedirs(BASE_DIR, exist_ok=True)
+
+
+def save_artifact(temp_file_path):
+
+    artifact_id = str(uuid.uuid4())
+
+    artifact_dir = os.path.join(BASE_DIR, artifact_id)
+
+    os.makedirs(artifact_dir, exist_ok=True)
+
+    filename = os.path.basename(temp_file_path)
+
+    final_path = os.path.join(artifact_dir, filename)
+
+    shutil.copy2(temp_file_path, final_path)
+
+    return artifact_id, final_path
